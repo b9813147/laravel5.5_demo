c9111f59eb95fde112917b17b362b403b121c17d
steven <b9813147@gmail.com>
2018-07-30 11:10:51 +0800

    create seeder 50

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,26 @@
+<?php /** @noinspection ALL */
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = factory(User::class)->times(50)->make();
+        User::query()->insert($users->makeVisible(['password', 'remember_token'])->toArray());
+
+        $user           = User::find(1);
+        $user->name     = 'ares';
+        $user->email    = 'ares@gmail.com';
+        $user->password = bcrypt('password');
+        $user->is_admin = true;
+        $user->save();
+
+    }
+}
